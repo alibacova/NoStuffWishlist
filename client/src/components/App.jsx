10576c3253033375834229391fe28bb2cedba1e6
alibacova <libacova.a@gmail.com>
2023-03-01 12:09:02 -0800

    Set up babel, webpack, initial index.jsx file

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+
+  return (
+    <div>Wishlist goes here</div>
+  );
+};
+
+export default App;
