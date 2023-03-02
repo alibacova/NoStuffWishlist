cf9b451805a9acb2210045fed267c4fd678d38c2
alibacova <libacova.a@gmail.com>
2023-03-01 19:43:22 -0800

    Set up context and refactor useEffect to use the context

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App.jsx';
+import { WishListContextProvider } from './context/WishListContext.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <WishListContextProvider>
+    <App />
+  </WishListContextProvider>
+);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useWishListContext } from '../hooks/useWishListContext.jsx';
 import axios from 'axios';
 import WishList from '../components/WishList.jsx';
 import WishForm from '../components/WishForm.jsx';
 
 const Home = () => {
-  const [wishList, setWishList] = useState(null);
+  const {wishList, dispatch} = useWishListContext();
 
   useEffect(() => {
     axios.get('/api/wishList')
-      .then((result) => setWishList(result.data))
+      .then((result) => dispatch({type: 'SET_WISHLIST', payload: result.data}))
       .catch((err) => console.error(err));
   }, []);
 
