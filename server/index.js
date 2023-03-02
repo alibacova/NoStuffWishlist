754a2e00e1070854645b29b6bb489e67a73fee3c
alibacova <libacova.a@gmail.com>
2023-03-02 14:22:09 -0800

    Refactor code to include new routes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-const routes = require('./routes.js');
+const wishesRoutes = require('./routes/wishes.js');
+const userRoutes = require('./routes/user.js');
 const mongoose = require('mongoose');
 
 const port = process.env.PORT;
@@ -12,7 +13,8 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/wishList', routes);
+app.use('/api/wishList', wishesRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect('mongodb://localhost:27017/wishlist')
   .then(() => {
