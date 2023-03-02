ab310c4c2a90f2bac1316895a1dbd1db42bf8cb6
alibacova <libacova.a@gmail.com>
2023-03-02 14:16:23 -0800

    Add validation for sign up request

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const wishSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -11,10 +13,37 @@ const wishSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+  if (!email || !password) {
+    throw Error('Please fill out both email and password fields');
+  };
+
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid');
+  };
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  };
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('A user with this email is already registered');
+  };
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = this.create({ email, password: hash });
+  return user;
+};
 
 const Wish = mongoose.model('wish', wishSchema);
 const User = mongoose.model('user', userSchema);
