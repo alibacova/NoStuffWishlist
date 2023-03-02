b9bfade69978c0e7a787c0f3be36a6c33d5e883e
alibacova <libacova.a@gmail.com>
2023-03-02 13:34:30 -0800

    Add materialUI theme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 import { WishListContextProvider } from './context/WishListContext.jsx';
+import Theme from './theme.jsx';
+import { ThemeProvider } from '@mui/material';
+
+// import { AuthContextProvider } from './context/AuthContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-  <WishListContextProvider>
-    <App />
-  </WishListContextProvider>
+  // <AuthContextProvider>
+  <ThemeProvider theme={Theme}>
+    <WishListContextProvider>
+      <App />
+    </WishListContextProvider>
+  </ThemeProvider>
+  // </AuthContextProvider>
 );
