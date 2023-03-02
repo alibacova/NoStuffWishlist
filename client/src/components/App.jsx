70593c697ba44d45d60a921c186c9d9849fd041d
alibacova <libacova.a@gmail.com>
2023-03-02 13:25:51 -0800

    Rearrange routes, add material ui components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
 import Home from '../pages/Home.jsx';
+import { Container, Typography } from '@mui/material';
+// import SignupForm from './SignupForm.jsx';
 
 const App = () => {
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Navbar />
-        <h1>Alex's Wishlist</h1>
-        <div className="pages">
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </div>
+        <Container sx={{bgcolor: '#3BCEAC', height: '100vh'}}>
+          <Navbar />
+          <Typography variant='h3' sx={{my: 4, color: 'primary.main'}}>Alex's Wishlist</Typography>
+          <div className="pages">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              {/* <Route path='/signup' component={<SignupForm />} /> */}
+            </Routes>
+          </div>
+        </Container>
       </BrowserRouter>
     </div>
   );
