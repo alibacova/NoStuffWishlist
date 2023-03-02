d86e5a8b657662f946f9035c2655b1fd56accfcc
alibacova <libacova.a@gmail.com>
2023-03-01 18:13:50 -0800

    Implement working delete, edit and reserve functionality

--- client/src/components/Wish.jsx
+++ client/src/components/Wish.jsx
@@ -1,21 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import WishForm from './WishForm.jsx';
+import ReserveWishForm from './ReserveWishForm.jsx';
 
 const Wish = ({ wish }) => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [error, setError] = useState(null);
+  const [isReserved, setIsReserved] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [showReserveForm, setShowReserveForm] = useState(false);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios.delete(`/api/wishList/${wish._id}`)
+      .then((result) => console.log(result))
+      .catch((err) => setError(err));
+    setError(null);
+  };
+
   return (
     <div>
-      <h4>{wish.title}</h4>
+      <h4 onClick={() => setIsOpen(!isOpen)}>{wish.title}</h4>
       {isOpen &&
         <>
           <p>{wish.description}</p>
           <p>{wish.url ? wish.url : ''}</p>
         </>
       }
-      <button>Delete</button>
-      <button>Edit</button>
-      <button>Reserve</button>
+      <button onClick={handleDelete}>Delete</button>
+      <button onClick={() => setShowEdit(!showEdit)}>Edit</button>
+      {showEdit && <WishForm wish={wish} type='edit' setShowEdit={setShowEdit}/>}
+      {(!isReserved || wish.reserved === false) && <button onClick={() => setShowReserveForm(true)}>Reserve</button>}
+      {(isReserved || wish.reserved === true) && <button disabled={true}>Reserved</button>}
+      {showReserveForm && <ReserveWishForm wish={wish} setIsReserved={setIsReserved} setShowReserveForm={setShowReserveForm}/>}
     </div>
   );
 };
