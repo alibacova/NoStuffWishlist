860cb543e6eb2ed86a2cef0b3838dcce7ee47a48
alibacova <libacova.a@gmail.com>
2023-06-01 19:35:17 -0700

    Add more wishes

--- seedData.js
+++ seedData.js
@@ -11,5 +11,28 @@ const wishes = [
     "username" : "tapushka",
     "title": "Hike in Redwood Regional Park",
     "description": "Let's go for a hike. I haven't hiked in ages!"
-}
+  },
+  {
+    "username" : "tapushka",
+    "title": "National Parks Annual Pass",
+    "description": "For all the adventures"
+    "url": "https://store.usgs.gov/recreational-passes"
+ },
+ {
+   "username" : "tapushka",
+   "title": "Cooking party and dinner",
+   "description": "Lets get together, make some amazing food and then share this amazing meal!"
+  },
+  {
+    "username" : "tapushka",
+    "title": "Exploratorium membership",
+    "description": "After Dark Thursday nights are the best! really want to go there more often"
+    "url": "https://www.exploratorium.edu/membership"
+  },
+  {
+    "username" : "tapushka",
+    "title": "Redwood Canopy Tour",
+    "description": "I've had it on my wishlist for so looooong"
+    "url": "https://mounthermonadventures.com/"
+  },
 ];
