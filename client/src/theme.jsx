53764c75719a56a6ca97ad6d5da927475131bde8
alibacova <libacova.a@gmail.com>
2023-06-01 19:34:14 -0700

    Change colors and fonts

--- client/src/theme.jsx
+++ client/src/theme.jsx
@@ -3,38 +3,32 @@ import { ThemeProvider, createTheme } from '@mui/material';
 const Theme = createTheme({
   palette: {
     primary: {
-      main: '#540D6E',
-      light: '#8C15B7',
-      dark: '#460B5B',
+      main: '#8338EC',
     },
     secondary: {
-      main: '#3BCEAC',
-      light: '#80FFDB',
-      dark: '#64DFDF',
+      main: '#FB5607',
     },
-    text: {
-      primary: '#FFD23F',
-      secondary: '#6930C3',
-    }
-
-
   },
   typography: {
     fontFamily: [
       'Urbanist',
       'sans-serif'
     ].join(','),
+    h2: {
+      fontSize: '2.5rem',
+      fontWeight: 700,
+    },
     h3: {
       fontSize: '1.5rem',
       fontWeight: 600,
     },
     h6: {
       fontSize: '1.2rem',
-      fontWeight: 400,
+      fontWeight: 600,
     },
     body1: {
       fontSize: '1rem',
-      fontWeight: 200,
+      fontWeight: 400,
     }
 
   }
