909277b3559a3d431cb035afdc92330f916b1498
alibacova <libacova.a@gmail.com>
2023-11-02 15:42:24 -0700

    Add middleware to require auth for wishes routes

--- server/routes/wishes.js
+++ server/routes/wishes.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const router = express.Router();
+const requireAuth = require("../middleware/requireAuth.js");
 const controllers = require("../controllers/wishes.js");
+
+const router = express.Router();
+router.use(requireAuth);
 
 router.get("/", controllers.getWishList);
 router.get("/:wid", controllers.getWish);
