5eed3dda4068f34909276a70212244e68ffee4b6
alibacova <libacova.a@gmail.com>
2023-11-02 17:34:39 -0700

    Set up protected routes, and switch to HashRouter as a temporary workaround

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,37 +1,37 @@
-import React, {
-  useState,
-  useEffect,
-} from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import Navbar from './Navbar.jsx';
-import Home from '../pages/Home.jsx';
-import {
-  Container,
-  Typography,
-} from '@mui/material';
-import SignupForm from './SignupForm.jsx';
-import LoginForm from './LoginForm.jsx';
+import React, { useState, useEffect } from "react";
+import { HashRouter, Navigate, Routes, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar.jsx";
+import Home from "../pages/Home.jsx";
+import { Container, Typography } from "@mui/material";
+import Signup from "../pages/Signup.jsx";
+import LoginForm from "./LoginForm.jsx";
+import { useAuthContext } from "../hooks/useAuthContext.js";
 
 const App = () => {
-
+  const { user } = useAuthContext();
   return (
-    <Container sx={{bgcolor: '#FFBE0B', height: '100vh'}}>
+    <Container sx={{ bgcolor: "#FFBE0B", height: "100vh" }}>
       <div className="app">
-        <BrowserRouter>
+        <HashRouter>
           <Navbar />
           <div className="pages">
             <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route path='/signup' element={<SignupForm />} />
-              <Route path='/login' element={<LoginForm />} />
+              <Route
+                exact
+                path="/"
+                element={user ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <LoginForm /> : <Navigate to="/" />}
+              />
             </Routes>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </Container>
   );
